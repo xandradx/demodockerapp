25bb30c9554ec73937dcd3a33119939f3b63a348
Luis Pérez <luis.luimarin@gmail.com>
2017-02-01 17:51:33 -0600

    add ip detection

--- demodockerapp.py
+++ demodockerapp.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import netifaces as ni
 import socket
 
 app = Flask(__name__)
 Bootstrap(app)
 
+def get_ipaddress():
+    ip = None
+    for iface in ni.interfaces():
+        if iface == 'lo0':
+            continue
+        ip = ni.ifaddresses(iface)[2][0]['addr']
+    return ip
+
 @app.route('/')
 @app.route('/index')
 def index():
     hostname = socket.gethostname()
-    return render_template('index.html', hostname=hostname)
-
+    ip = get_ipaddress()
+    return render_template('index.html', hostname=hostname, ip=ip)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
